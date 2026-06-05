dd1d0e5f025bb657550f1f74ede5aa19d0e4ee77
Abdullah <abdullahmug2332@gmail.com>
2026-06-05 16:11:13 +0500

    add uploads folder in gitignore

--- lib/db.ts
+++ lib/db.ts
@@ -1,24 +1,24 @@
 import mysql from 'mysql2/promise';
 
-// const pool = mysql.createPool({
-//   host: 'localhost',
-//   user: 'root',
-//   password: '',
-//   database: 'hayaibu_talent',
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
-
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'backend_user',
-  password: 'admin@123',
+  user: 'root',
+  password: '',
   database: 'hayaibu_talent',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
+
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'backend_user',
+//   password: 'admin@123',
+//   database: 'hayaibu_talent',
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
 
 export default pool;
